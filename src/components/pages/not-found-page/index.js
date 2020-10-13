e08b498bc1c30db04e85a799909f20808b23572f
framista <adajab1@gmail.com>
2020-10-13 20:07:42 +0200

    Update not found page

--- src/components/pages/not-found-page/index.js
+++ src/components/pages/not-found-page/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 const NotFoundPage = () => {
   return (
     <Container className="text-dark">
       <p className="mt-5 display-3 text-center font-weight-bold">404</p>
-      <h1 className="my-5  text-center text-uppercase">Strona niedostępna</h1>
+      <h1 className="my-5  text-center text-uppercase">Site not found</h1>
       <h3 className="text-center">
-        Niestety podany przez Ciebie adres strony nie jest dostępny
+        Unfortunately the website is not available.
       </h3>
       <div className="mt-5 text-center">
-        <Link
-          to="/"
-          className="text-primary btn btn-primary text-light text-uppercase"
-        >
-          Strona główna
+        <Link to="/">
+          <Button className="task__btn task__btn--primary">Home Page</Button>
         </Link>
       </div>{' '}
     </Container>
