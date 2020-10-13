1fc7ada286a8eed02337455e5a9f7d5929eba0fd
framista <adajab1@gmail.com>
2020-10-13 20:21:26 +0200

    Updates

---
+++ precache-manifest.89bbaa787da28d1de4b2c83d2d7f9449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5bcec8c562ecd370c62b8863c69133",
+    "url": "/listtable/index.html"
+  },
+  {
+    "revision": "6d661259afbbb8a04218",
+    "url": "/listtable/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5e1b64607df3a71a9142",
+    "url": "/listtable/static/css/main.657019bc.chunk.css"
+  },
+  {
+    "revision": "6d661259afbbb8a04218",
+    "url": "/listtable/static/js/2.617e51d6.chunk.js"
+  },
+  {
+    "revision": "6807ca1bc4f53c4e63d83a23e9a381a7",
+    "url": "/listtable/static/js/2.617e51d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1b64607df3a71a9142",
+    "url": "/listtable/static/js/main.79c940a9.chunk.js"
+  },
+  {
+    "revision": "d432cbb285259daaa03f",
+    "url": "/listtable/static/js/runtime-main.c2c72025.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/listtable/precache-manifest.89bbaa787da28d1de4b2c83d2d7f9449.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/listtable/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
